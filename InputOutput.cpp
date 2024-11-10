e78e2e711ed4f03b1e4e8e6ddd48aef31be2b5b5
72Rahul <72rahulpal@gmail.com>
2024-11-10 14:34:11 +0530

    Add files via upload

---
+++ InputOutput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char arr[100];
+    cin >> arr;
+    cout << arr[1] << endl;
+    cout << arr[3] << endl;
+    cout << arr[4] << endl;
+    
+}
